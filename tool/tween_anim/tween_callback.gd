1355b66b53a87cef746875da49b07a4379ed219b
nenado <17idontwant@gmail.com>
2026-02-19 16:25:19 +0200

    Delete

--- tool/tween_anim/tween_callback.gd
+++
@@ -1,7 +0,0 @@
-extends TweenDelay
-class_name TweenCallback
-
-@export var object: NodePath
-@export var parallel: bool
-@export var method: StringName
-@export var bind : Array[Variant]
--- tool/tween_anim/tween_resource.gd
+++
@@ -1,9 +0,0 @@
-extends TweenTweener
-class_name TweenResource
-
-@export var object: NodePath
-@export var settings: TweenSettings
-@export var property: Array[TweenTweener]
-
-func create(template: TweenResource = self) -> Tween:
-	return TweenAnim.create(template)
